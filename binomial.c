74925650535f373f3e4c5cabbb7c817ab76c1230
Billy <wxb107@case.edu>
2015-09-12 14:38:57 -0400

    First child and message.

--- binomial.c
+++ binomial.c
@@ -2,10 +2,30 @@
 #include<stdlib.h>
 #include<unistd.h>
 
+// Largest 32 bit integer
+#define LOOP_ITERATIONS (0x7fffffff)
+
 int main() {
 
-	pid_t fork_results;
+	int i;
+	pid_t fork_result;
+	
+	fork_result = fork();
 
+	if (fork_result == 0) {
+		
+		printf("(n (n-2)) binomial coefficient computations of integers n=2, 3, 10, start now!\n");
+		for(i = 0; i < LOOP_ITERATIONS; i++) { }
+		return EXIT_SUCCESS;
+	}
+	else if (fork_result > 0) {
+		wait();
+		printf("I am the parent\n");
+	}
+	else {
+		perror("fork() failed");	
+		return EXIT_FAILURE;
+	}
  
-
+	return EXIT_SUCCESS;
 }
