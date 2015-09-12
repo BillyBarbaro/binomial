085c82dd7b3036d862bf5c91b93181701f346a4e
Billy <wxb107@case.edu>
2015-09-12 15:52:19 -0400

    Added running time info before process termination.

--- binomial.c
+++ binomial.c
@@ -1,12 +1,29 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
+#include<time.h>
+#include<sys/times.h>
 
 // Largest 32 bit integer
 #define LOOP_ITERATIONS (0x7fffffff)
 
-void print_process_info() {
-	
+void print_termination_info() {
+	struct tms process_time;
+	clock_t result;
+	double user_time_seconds;
+	double system_time_seconds;
+
+	result = times(&process_time);
+	if (result == (clock_t) -1) {
+		perror("times() failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	user_time_seconds = (double)process_time.tms_utime / CLOCKS_PER_SEC;
+	system_time_seconds = (double)process_time.tms_stime / CLOCKS_PER_SEC;
+
+	printf("User time: %f seconds\n", user_time_seconds);
+	printf("System time: %f seconds\n", system_time_seconds);
 }
 
 void print_termination_status(pid_t pid, int status) {
@@ -25,7 +42,6 @@ pid_t print_start_message() {
 	if (fork_result == 0) {
 		printf("(n (n-2)) binomial coefficient computations of integers n=2, 3, 10, start now!\n");
                 for(i = 0; i < LOOP_ITERATIONS; i++) { }
-		return EXIT_SUCCESS;
 	}
 	else if (fork_result > 0) { 
 		terminated_pid = wait(&status);
@@ -33,6 +49,7 @@ pid_t print_start_message() {
 	}
 	else {
 		perror("fork() failed");
+		exit(EXIT_FAILURE);
 	}
 	return fork_result;
 }
@@ -43,12 +60,11 @@ int main() {
 	
 	start_result = print_start_message();
 
-	if (start_result == 0) {	
+	// If it's the child process, don't continue
+	if (start_result == 0) {
+		printf("First chilld preparing to terminate.\n");
+		print_termination_info();	
 		return EXIT_SUCCESS;
 	}
-	else if (start_result < 0) {
-		return EXIT_FAILURE;
-	}
- 
 	return EXIT_SUCCESS;
 }
